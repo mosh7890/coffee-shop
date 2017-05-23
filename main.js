d5665f81918e72641161dd2457fff0b8faf3031d
mosh7890 <mosh7890@gmail.com>
2017-05-23 16:57:33 +0300

    fixed code

--- main.js
+++ main.js
@@ -1,20 +1,56 @@
 var coffeeShop = {
   beans: 40,
+  money: 100,
 
   drinkRequirements: {
-    latte: 10,
-    americano: 5,
-    doubleShot: 15,
-    frenchPress: 12
+    latte: {
+      beanCost: 10,
+      price: 20
+    },
+    americano: {
+      beanCost: 5,
+      price: 10
+    },
+    doubleShot: {
+      beanCost: 15,
+      price: 30
+    },
+    frenchPress: {
+      beanCost: 12,
+      price: 24
+    }
   },
 
   makeDrink: function (drinkType) {
-    // TODO: Finish this method
+    if (!(drinkType in coffeeShop.drinkRequirements)) {
+      console.log("Sorry, we don't make " + drinkType);
+      return;
+    }
+    if (this.drinkRequirements[drinkType].beanCost <= this.beans) {
+      this.beans -= this.drinkRequirements[drinkType].price;
+      this.money += this.drinkRequirements[drinkType].price;
+    }
+    else {
+      console.log("sorry, no beans");
+    }
+    console.log(this.beans);
+  },
+
+  buySupplies: function (quantity) {
+    this.beans += quantity;
+    this.money -= quantity;
+  },
+
+  buyDrink: function (request) {
+    this.makeDrink(request);
   }
 }
-
-coffeeShop.makeDrink("latte");
-coffeeShop.makeDrink("americano");
-coffeeShop.makeDrink("filtered");
-coffeeShop.makeDrink("doubleShot");
-coffeeShop.makeDrink("frenchPress");
+console.log("Money: " + coffeeShop.money);
+coffeeShop.buySupplies(50);
+console.log("Money: " + coffeeShop.money);
+coffeeShop.buyDrink("latte");
+coffeeShop.buyDrink("americano");
+coffeeShop.buyDrink("filtered");
+coffeeShop.buyDrink("doubleShot");
+coffeeShop.buyDrink("frenchPress");
+console.log("Money: " + coffeeShop.money);
